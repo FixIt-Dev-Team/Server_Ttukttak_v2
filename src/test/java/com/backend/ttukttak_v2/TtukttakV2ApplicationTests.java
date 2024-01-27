10e5274935fc79334be8e42893476d51e13b80d7
destiny3912 <destiny3910@gmail.com>
2024-01-27 17:09:10 +0900

    project init

---
+++ src/test/java/com/backend/ttukttak_v2/TtukttakV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ttukttak_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtukttakV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
