e5e4274906aab5f37061307f8cd6152e82e9da85
wgnw <m4a11carbin4@gmail.com>
2024-02-20 15:30:12 +0900

    gcloud cli test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 docker rm -f ttukttak_redis
 docker pull redis
-docker run -p 6379:6379 --name ttukttak_redis redis
+docker run -d -p 6379:6379 --name ttukttak_redis redis
 
 docker rm -f ttukttak
 docker pull openjdk:17-jdk-alpine
-docker run -p 8080:8080 --name ttukttak openjdk:17-jdk-alpine
+docker run -d -p 8080:8080 --name ttukttak openjdk:17-jdk-alpine
